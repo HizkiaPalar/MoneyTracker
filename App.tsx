889cf758e7a2ecfaca6c37b3c4ac93835ca905bb
HizkiaPalar <Hizkiapalar294@gmail.com>
2024-03-05 21:35:48 +0800

    slicing

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import React from 'react';
+import SplashScreen from './src/pages/SplashScreen';
+
+const App = () => {
+  return <SplashScreen/>;
+};
+
+export default App ;
+
+const  styles = StyleSheet.create({});
