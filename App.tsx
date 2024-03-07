16a2100d1e21cbb26434b6d498c26d738bd2e396
HizkiaPalar <Hizkiapalar294@gmail.com>
2024-03-07 21:52:55 +0800

    adding navigate

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
-import { StyleSheet } from 'react-native';
 import React from 'react';
-import SplashScreen from './src/pages/SplashScreen';
+import {NavigationContainer} from '@react-navigation/native';
+import Router from './src/router/index';
 
 const App = () => {
-  return <SplashScreen/>;
+  return (
+    <NavigationContainer>
+      <Router />
+    </NavigationContainer>
+  );
 };
 
-export default App ;
-
-const  styles = StyleSheet.create({});
+export default App;
---
+++ src/pages/index.tsx
@@ -0,0 +1,4 @@
+import SplashScreen from "./SplashScreen";
+import Signin from './Signin';
+
+export {SplashScreen, Signin}
